b46ffbcb984f76dd8048ae63226171f5179d0cf8
arubalcaba001 <45408763+arubalcaba001@users.noreply.github.com>
2019-04-17 21:08:04 -0700

    clean it up for one function

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+from io import StringIO
+
 
 # Create an instance of Flask app
 app = Flask(__name__)
@@ -14,30 +16,23 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.collection.find_one()
-
+    mars_information = mongo.db.collection.find_one()
+ 
     # Return template and data
-    return render_template("index.html", mars=mars_information)
+    return render_template("index.html", mars_information=mars_information)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_information = mongo.db.mars_information
-    mars_data = scrape_mars.scrape_mars_news()
-    mars_data = scrape_mars.scrape_feature_image()
-    mars_data = scrape_mars.scrape_mars_facts()
-    mars_data = scrape_mars.scrape_mars_weather()
-    mars_data = scrape_mars.scrape_mars_hemisphere()
-    mars_information.update({}, mars_data, upsert=True)
+    mars_data = scrape_mars.mars_scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
